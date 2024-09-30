15fc5b0dcc33fc25f0df358907ca42dc220ecd12
karell606 <karell.azabe@epitech.digital>
2024-09-30 13:55:05 +0200

    ex_08.js

---
+++ ex_08.js
@@ -0,0 +1,8 @@
+if (breadCount >= 2 && hamCount >= 1 && tunaCount >= 1) {
+	displayThisText ("I can make a royal sandwhich!")
+ } else if (breadCount >= 2 && hamCount >= 1) {
+	displayThisText ("I can make a ham sandwhich!")
+ } else if (breadCount >= 2 && tunaCount >= 1) {
+	displayThisText ("I can make a tuna sandwhich!")
+ } else {
+  displayThisText ("I'd rather be fasting tonight..") }
