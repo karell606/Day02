9a36f744c27cb62bfd0693dc7c4a1417d56234c2
karell606 <karell.azabe@epitech.digital>
2024-09-30 15:53:04 +0200

    ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,7 +1,7 @@
 displayThisText ("The variable value is") + (input_var)
 if (Number.isInteger(input_var)) {
 	displayThisText("Its type is integer")
- } else if {(Number.isFinite(input_var)) {
+ } else if (Number.isFinite(input_var)) {
  	displayThisText("its type is float")
  } else {
  	displayThisText ("Its type is" + typeof input_var) 
