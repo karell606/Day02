c4dc9325816757ac1fd767cebebfbd0fc9d8c2e7
karell606 <karell.azabe@epitech.digital>
2024-09-30 12:12:34 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,7 @@
+const firstname = "John"
+const lastname = "Doe"
+const cardCode = "1111 2222 3333 4444 "
+const expirydateMonth = 21
+const expirydateYear = 2042
+let computerbankInfo = firstname+ "," +lastname+ "," +cardCode+ "," +expirydateMonth+ "/" +expirydateYear;
+displayInCard(computerbankInfo);
