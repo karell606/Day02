6e1ad0b19a1c98793dd23351c9cbb2e63c6636b6
karell606 <karell.azabe@epitech.digital>
2024-09-30 15:24:19 +0200

    ex_09.js

---
+++ ex_09.js
@@ -0,0 +1,11 @@
+displayThisText ("The variable value is") + (input_var)
+if (Number.isInteger(input_var)) {
+	displayThisText("Its type is integer")
+ } else if {(Number.isFinite(input_var)) {
+ 	displayThisText("its type is float")
+ } else {
+ 	displayThisText ("Its type is" + typeof input_var) 
+	}
+if (input_var ==42) {
+	displayThisText("It is the meaning of life");
+  }
