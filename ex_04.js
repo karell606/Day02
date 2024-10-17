94dee9d571fcf8fe8083cf8b55dec429f0fe163b
karell606 <karell.azabe@epitech.digital>
2024-10-17 11:00:26 +0200

    ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,14 +1,7 @@
-typeof x // Returns undefined
-displayThisText(typeof x);
-typeof true // Returns boolean 
+displayThisText(typeof abcdefg);
 displayThisText(typeof true);
-typeof 42 // Returns number
 displayThisText(typeof 42);
-typeof 1234n // Returns bigint
-displayThisText(typeof 1234n);
-typeof Symbol()// Returns symbol
-displayThisText(typeof Symbol());
-typeof null // Returns object
+displayThisText(typeof 123456789n);
+displayThisText(typeof Symbol('foo'));
 displayThisText(typeof null);
-typeof displayThisText// Returns function
 displayThisText(typeof displayThisText);
